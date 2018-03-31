fc33eb18e46c02c8a6106f6d34ed345204e89191
Administrator <252932110@qq.com>
2018-04-01 02:00:48 +0800

    yin yue app

---
+++ vue/src/common/js/dom.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2018/3/22.
+ */
+// export function addClass(el, className) {
+//   if (hasClass(el, className)) {
+//     return
+//   }
+//   let newClass = el.className.split(' ')
+//   newClass.push(className)
+//   el.className = newClass.join(' ')
+// }
+//
+// export function hasClass(el, className) {
+//   let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+//   return reg.test(el.className)
+// }
+export function addClass(el, className) {
+  el.classList.add(className)
+}
+
+export function hasClass(el, className) {
+  return el.classList.contains(className)
+}
